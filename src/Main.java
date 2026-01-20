7a487d32ce1d2d93afcb20edd2e3a89a73e56306
chhi-hiro <chhunhourtiek255@gmail.com>
2026-01-20 14:45:37 +0700

    vehice class

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main{
     public static void main(){
         System.out.println("hello");
         System.out.println("Welcome to Java!");
+        Vehicle v1 = new Vehicle(1,"gasoline", "SUV", "Ford", "Escape",30000,true);
+        System.out.println(v1.toString());
     }
 }
---
+++ src/Vehicle.java
@@ -0,0 +1,85 @@
+public class Vehicle {
+    private int vehicleId;
+    private String powertrain, vehicleClass, brand, model;
+    private double price;
+    private boolean isAvailable;
+
+    public Vehicle(int vehicleId, String powertrain, String vehicleClass, String brand, String model, double price, boolean isAvailable) {
+        this.vehicleId = vehicleId;
+        this.powertrain = powertrain;
+        this.vehicleClass = vehicleClass;
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+        this.isAvailable = isAvailable;
+    }
+
+    public int getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(int vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public String getPowertrain() {
+        return powertrain;
+    }
+
+    public void setPowertrain(String powertrain) {
+        this.powertrain = powertrain;
+    }
+
+    public String getVehicleClass() {
+        return vehicleClass;
+    }
+
+    public void setVehicleClass(String vehicleClass) {
+        this.vehicleClass = vehicleClass;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "vehicleId=" + vehicleId +
+                ", powertrain='" + powertrain + '\'' +
+                ", vehicleClass='" + vehicleClass + '\'' +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", price=" + price +
+                ", isAvailable=" + isAvailable +
+                '}';
+    }
+}
